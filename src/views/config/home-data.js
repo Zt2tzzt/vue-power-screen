c0ebf5bfa19aa512445a9070e57264eeed8b772d
ZeT1an <584633819@qq.com>
2023-01-26 22:26:08 +0800

    fix: v1;

--- src/views/config/home-data.js
+++ src/views/config/home-data.js
@@ -1,108 +1,108 @@
 // 饼图-充电桩饱和比例
 export const chargingPileData = [
-  {
-    value: 0,
-    name: "广州占比",
-    percentage: "0%",
-    color: "#34D160",
-  },
-  {
-    value: 0,
-    name: "深圳占比",
-    percentage: "0%",
-    color: "#027FF2",
-  },
-  {
-    value: 0,
-    name: "东莞占比",
-    percentage: "0%",
-    color: "#8A00E1",
-  },
-  {
-    value: 0,
-    name: "佛山占比",
-    percentage: "0%",
-    color: "#F19610",
-  },
-  {
-    value: 0,
-    name: "中山占比",
-    percentage: "0%",
-    color: "#6054FF",
-  },
-  {
-    value: 0,
-    name: "珠海占比",
-    percentage: "0%",
-    color: "#00C6FF",
-  },
+	{
+		value: 0,
+		name: '广州占比',
+		percentage: '0%',
+		color: '#34D160'
+	},
+	{
+		value: 0,
+		name: '深圳占比',
+		percentage: '0%',
+		color: '#027FF2'
+	},
+	{
+		value: 0,
+		name: '东莞占比',
+		percentage: '0%',
+		color: '#8A00E1'
+	},
+	{
+		value: 0,
+		name: '佛山占比',
+		percentage: '0%',
+		color: '#F19610'
+	},
+	{
+		value: 0,
+		name: '中山占比',
+		percentage: '0%',
+		color: '#6054FF'
+	},
+	{
+		value: 0,
+		name: '珠海占比',
+		percentage: '0%',
+		color: '#00C6FF'
+	}
 ]
 
-// 流程监控的数据
+// 折线图-流程监控的数据
 export const processMonitoringData = [
-  {
-      "name": "正常",
-      "data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  },
-  {
-      "name": "异常",
-      "data": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  }
+	{
+		name: '正常',
+		data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	},
+	{
+		name: '异常',
+		data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	}
 ]
 
-// 充电数据统计
+// 柱状图-充电数据统计
 export const chargingStatisticsData = [
-  {
-      "name": "一月",
-      "value": 0
-  },
-  {
-      "name": "二月",
-      "value": 0
-  },
-  {
-      "name": "三月",
-      "value": 0
-  },
-  {
-      "name": "四月",
-      "value": 0
-  },
-  {
-      "name": "五月",
-      "value": 0
-  },
-  {
-      "name": "六月",
-      "value": 0
-  },
-  {
-      "name": "七月",
-      "value": 0
-  }
+	{
+		name: '一月',
+		value: 0
+	},
+	{
+		name: '二月',
+		value: 0
+	},
+	{
+		name: '三月',
+		value: 0
+	},
+	{
+		name: '四月',
+		value: 0
+	},
+	{
+		name: '五月',
+		value: 0
+	},
+	{
+		name: '六月',
+		value: 0
+	},
+	{
+		name: '七月',
+		value: 0
+	}
 ]
 
 // 异常监控
 export const exceptionMonitoringData = [
-  {
-      "id": 1,
-      "name": "异常1",
-      "value": 5,
-      "dur": "10s",
-      "begin": "0s"
-  },
-  {
-      "id": 2,
-      "name": "异常2",
-      "value": 3,
-      "dur": "10s",
-      "begin": "-3s"
-  },
-  {
-      "id": 3,
-      "name": "异常3",
-      "value": 5,
-      "dur": "10s",
-      "begin": "-5s"
-  }
+	{
+		id: 1,
+		name: '异常1',
+		value: 5,
+		dur: '10s',
+		begin: '0s'
+	},
+	{
+		id: 2,
+		name: '异常2',
+		value: 3,
+		dur: '10s',
+		begin: '-3s'
+	},
+	{
+		id: 3,
+		name: '异常3',
+		value: 5,
+		dur: '10s',
+		begin: '-5s'
+	}
 ]
